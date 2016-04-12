4954de16001af2aa5a51cf07773a482cc3738607
Jörgen Thelin <info@craven-studio.com>
2016-04-12 15:44:05 +0200

    Adding dynamic markup

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,51 @@
+(function( menu, $, config, undefined ) { 
+    "use strict"
+    
+    var containers = [];
+    
+    menu.init = function (container) {
+        containers = $(container);
+        
+        $.each(containers, function () {
+            var env = getEnvironment(this);
+            console.log("appending h2 to: ", this);
+            $(this).append("<h2>" + env.title + "</h2>");
+            
+            // todo: add ul
+$('<div>',
+{
+    id: 'test',
+    name: 'test',
+    class: 'test-class'
+}).css(
+{
+    width: '100px',
+    height: '100px',
+    backgroundColor: '#fff'
+});            
+        });
+    };
+    
+    function getEnvironment(container) {
+            var env = $(container).attr("data-config-environment");
+            return config[env];
+    };
+    
+    function lookup (array, prop, value) {
+        for (var i = 0, len = array.length; i < len; i++) {
+            if (array[i] && array[i][prop] === value) return array[i];
+        }
+
+        return null;
+    };
+}( window.menu = window.menu || {}, jQuery, dashboardConfig ));
+
+try { 
+  menu.init(".menu");
+} catch( e ) { 
+  console.log( e.message ); 
+}
+
 /*
     * If element has class .flyout, look for button in li element and attach click event to toggle ul list 
     * ul.flyout > li > <button> 
@@ -23,10 +71,9 @@
         });
     };
     
-    removeClass = function () {
+    function removeClass () {
         $(containers).removeClass('factive');
     }
-
 }( window.flyout = window.flyout || {}, jQuery ));
 
 try { 
