258b439e51cbc726e9a7f8d11076a353e142eadd
papa-ogen <thelin.jorgen@gmail.com>
2016-11-07 11:42:45 +0100

    Refactoring element creation

--- htdocs/js/db-links.js
+++ htdocs/js/db-links.js
@@ -1,29 +1,8 @@
 if (typeof dashboardConfig === "undefined") {
 
-    var exampleFormat = "<pre>var dashboardConfig = [\n" +
-                        "\t{\n" +
-        "\t\ttitle: \"Developer Links\",\n" +
-        "\t\tcredentials: [\n" +
-            "\t\t{\n" +
-                "\t\t\tid: \"teamcity\",\n" +
-                "\t\t\tapp: \"Team City\",\n" +
-                "\t\t\turl: [\"https://www.jetbrains.com/teamcity/\"],\n" +
-                "\t\t\tusername: \"username\",\n" +
-                "\t\t\tpassword: \"password\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-                "\t\t\tid: \"octopus\",\n" +
-                "\t\t\tapp: \"Octopus\",\n" +
-                "\t\t\turl: [\"https://octopus.com/\"],\n" +
-                "\t\t\tusername: \"username\",\n" +
-                "\t\t\tpassword: \"password\"\n" +
-            "\t\t}\n" +
-            "\t\t]\n" +
-            "\t}\n" +
-            "]" +
-            "</pre>";
     var dbApp = document.getElementById("db-app");
-    dbApp.innerHTML = "<div><strong>Error!</strong> Please add a <strong>config.js</strong> file to the project. <br /><br />Example: " + exampleFormat + "</div>";
+    dbApp.innerHTML = "<div><strong>Error!</strong> Please add a <strong>config.js</strong> file to the project. <br /><br />" +
+                      "Go to <a href=\"https://github.com/papa-ogen/DashBoard/blob/master/README.md\" target=\"self\">DashBoard on GitHub</a> for more information and correct syntax.</div>";
     dbApp.className += " db-error";
 
 } else {
@@ -39,19 +18,19 @@ if (typeof dashboardConfig === "undefined") {
         config.forEach(addMarkup);
 
         function addMarkup (element, index) {
-            var col = createElement("div", "db-size-1of2 db-links-collection", "", c);
-            var subCol = createElement("div", "db-grid-content", "", col);
-            var header = createElement("header", "db-links-header db-header-color-blue", "", subCol);
-            var title = createElement("h2", "", element.title, header);
-            var linkList = createElement("ul", "db-links", "", subCol);
+            var col = _createElement ("div", { classList: "db-size-1of2 db-links-collection" }, c);
+            var subCol = _createElement ("div", { classList: "db-grid-content" }, col);
+            var header = _createElement ("header", { classList: "db-links-header db-header-color-blue" }, subCol);
+            var title = _createElement ("h2", { text: element.title }, header);
+            var linkList = _createElement ("ul", { classList: "db-links" }, subCol);
 
             for(var i=0; i < element.credentials.length; i++) {
-                var li = createElement("li", "", "", linkList);
-                var label = createElement("label", "", "", li, {type: "for", value: id+linkCount});
-                var h4 = createElement("h4", "", element.credentials[i].app, label);
-                var checkbox = createInput("checkbox", id+linkCount, "db-toggle", "", li);
-                var details = createElement("div", "db-details", "", li);
-                var links = createElement("ul", "", "", details);
+                var li = _createElement ("li", {}, linkList);
+                var label = _createElement ("label", { attr: [ [ "for", id+linkCount ] ] }, li);
+                var h4 = _createElement ("h4", { text: element.credentials[i].app }, label);
+                var checkbox = _createElement ("input", { classList: "db-toggle", attr: [ [ "type", "checkbox" ], [ "id", id+linkCount ] ] }, li);
+                var details = _createElement ("div", { classList: "db-details" }, li);
+                var links = _createElement("ul", {}, details);
                 
                 addLinks(links, element.credentials[i]);
 
@@ -61,62 +40,86 @@ if (typeof dashboardConfig === "undefined") {
         };
 
         function addLinks (linkList, credentials) {
-            var li, label, ahref, input;
+            var li, label, ahref, input, url, username, password;
 
             if(credentials.url !== undefined) {
                 for(var i=0; i < credentials.url.length; i++) {
-                    li = createElement("li", "", "", linkList);
-                    label = createElement("label", "", "URL", li);
-                    ahref = createElement("a", "", credentials.url[i], li, {type: "href", value: "#"});
+                    url = credentials.url[i];
+                    username = credentials.username;
+                    password = credentials.password;
+
+                    if(url) {
+                        li = _createElement("li", {}, linkList);
+                        label = _createElement("label", { text: "URL" }, li);
+                        ahref = _createElement("a", { text: url, attr: [ [ "href", url ], [ "target", "self" ] ] }, li);
+                    }
                 }
             }
 
-            if(credentials.username !== undefined)  {
-                li = createElement("li", "", "", linkList);
-                label = createElement("label", "", "Username", li, {type: "for", value: "name"+credentialCount});
-                input = createInput("text", "name"+credentialCount, "", credentials.username, li);
+            if(username !== undefined && username)  {
+
+                li = _createElement ("li", {}, linkList);
+                
+                label = _createElement ("label", {
+                    text: "Username",
+                    attr: [ [ "for", "name"+credentialCount ] ]
+                }, li);
+                
+                input = _createElement ("input", {
+                    attr: [ [ "type", "text" ], [ "id", "name"+credentialCount ], [ "value", username ] ]
+                }, li);                 
+                
                 credentialCount++;
             }
 
-            if(credentials.password !== undefined) { 
-                li = createElement("li", "", "", linkList);
-                label = createElement("label", "", "Password", li, {type: "for", value: "name"+credentialCount});
-                input = createInput("password", "name"+credentialCount, "", credentials.password, li);
+            if(password !== undefined && password) { 
+                
+                li = _createElement ("li", {}, linkList);
+                
+                label = _createElement ("label", {
+                    text: "Password",
+                    attr: [ [ "for", "name"+credentialCount ] ]
+                }, li);
+
+                input = _createElement ("input", {
+                    attr: [ [ "type", "password" ], [ "id", "name"+credentialCount ], [ "value", password ] ]
+                }, li); 
+
                 credentialCount++;
             }
         };
 
-        function createElement (type, classList, text, appendTo, attribute = 0) {
-            var e = document.createElement(type);
-            // Todo: change to one object parameter, use switch to determine type of attr or classList
-            if(text.length > 0) {
-                var t = document.createTextNode(text);
+        /*
+            Create a HTML Object dynamically
+            Example: 
+            var obj = {
+                classList: "class1 class2",
+                attr: [ ["id", "123"], ["name", "test"] ]
+            };
+            var e = createElement("div", obj, li);
+        */
+        function _createElement (element, obj, appendTo) {
+            var e = document.createElement(element);
+
+            if(obj.text) {
+                var t = document.createTextNode(obj.text);
                 e.appendChild(t);
             } 
 
-            if(attribute) {
-                e.setAttribute(attribute.type, attribute.value);
+            if(obj.classList) {
+                e.classList = obj.classList;
             }
 
-            if(classList.length > 0) {
-                e.classList = classList;
+            if(obj.attr) {
+                for(var i = 0; i < obj.attr.length; i++) {
+                    e.setAttribute(obj.attr[i][0], obj.attr[i][1]);
+                }
             }
 
             appendTo.appendChild(e);
 
-            return e;
-        }
-
-        function createInput (type, id, classList, value, appendTo) {
-            // Todo: change to one object parameter, use switch to determine type of attr or classList
-            var e = document.createElement("input");
-            e.setAttribute("type", type);
-            e.setAttribute("id", id);
-            e.setAttribute("value", value);
-            e.classList = classList;
-
-            appendTo.appendChild(e);
-        }
+            return e;            
+        };
 
         // Todo: Add event receiver on focus text: http://stackoverflow.com/questions/480735/select-all-contents-of-textbox-when-it-receives-focus-javascript-or-jquery
 
