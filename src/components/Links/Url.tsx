efd1294c742abbd7aff7326eea18aceff5852e81
papa-ogen <thelin.jorgen@gmail.com>
2022-07-01 14:06:37 +0200

    link text

--- src/components/Links/Url.tsx
+++ src/components/Links/Url.tsx
@@ -2,7 +2,7 @@ const Url = ({ url }: { url: string }) => {
   return (
     <div class="flex pb-2">
       <p class="text-white pr-4">URL</p>
-      <a href={url} target="self" class="text-blue">
+      <a href={url} target="self" class="text-blue underline">
         {url}
       </a>
     </div>
