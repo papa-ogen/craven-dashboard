8173330df1115573db23b8ac500b1a7be940b221
Jörgen Thelin <jorthe@carnegie.se>
2022-09-26 08:38:47 +0200

    update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './components/App'
 
 import './index.css'
 
-const app = document.getElementById('app')
-ReactDOM.render(<App />, app)
+const container = document.getElementById('app')
+const root = createRoot(container)
+root.render(<App />)
