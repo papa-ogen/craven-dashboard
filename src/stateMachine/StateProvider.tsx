aaf18fbf76ccaebb492150b18c0f2cd13c99f7b6
Jörgen Thelin <jorthe@carnegie.se>
2022-10-25 10:39:33 +0200

    fixed some lint errors

--- src/stateMachine/StateProvider.tsx
+++ src/stateMachine/StateProvider.tsx
@@ -1,21 +1,15 @@
 import React, { createContext, useContext } from 'react'
-import { useInterpret, useMachine } from '@xstate/react'
-import { State, Interpreter } from 'xstate'
+import { useMachine } from '@xstate/react'
 
-import machine, { StateContext, StateEvent } from './machine'
-
-type Distribute<U, C> = U extends any ? { value: U; context: C } : never // util
-type StateSchema = { states: { some_state: Record<string, unknown> } }
-type TypeState = Distribute<keyof StateSchema['states'], StateContext> // assuming same context
-
-type Context = [
-  State<StateContext, StateEvent, StateSchema, TypeState>,
-  Interpreter<StateContext, StateSchema, StateEvent, TypeState>['send']
-]
+import machine from './machine'
 
 export const GlobalStateContext = createContext({} as any)
 
-export const GlobalStateProvider = ({ children }) => {
+export const GlobalStateProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
   const svc = useMachine(machine)
 
   return (
